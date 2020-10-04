f0b5dffd23397ab4ff82ebe94e8d08b92fd26da5
SriHarsha311 <61814657+SriHarsha311@users.noreply.github.com>
2020-10-04 16:19:41 +0530

    Update app.js

--- Minesweeper/app.js
+++ Minesweeper/app.js
@@ -1,41 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
     const n = 20;
-    document.addEventListener('contextmenu',
-        event => event.preventDefault());
+    document.addEventListener('contextmenu', event => event.preventDefault());
     var grid = document.getElementsByClassName('grid')[0];
     for (let i = 0; i < n * n; i++) {
         var d = document.createElement("div");
         d.setAttribute("id", i);
         grid.appendChild(d);
     }
-    const squares = document.querySelectorAll('.grid div');
+    grid.style.width = 600 + "px";
+    grid.style.width = 600 + "px";
+    var squares = document.querySelectorAll('.grid div');
     var arr = [];
-    var mines = [];
+    var flagged = [];
+    var mines = {};
+    var nom = 0;
+    var count = 0;
     squares.forEach(index => {
+        index.style.width = 28 + "px";
+        index.style.height = 28 + "px";
         arr.push(0);
         index.addEventListener('click', flip);
         index.addEventListener('contextmenu', flag);
     });
-
-    function flag() {
-        if (this.classList.contains('flag')) {
-            this.classList.remove('flag');
-            this.addEventListener('click', flip);
-        } else {
-            this.classList.add('flag');
-            this.removeEventListener('click', flip);
-        }
-    }
     for (let i = 0; i < n * n / 7; i++) {
         const rn = Math.floor(Math.random() * n * n);
         if (arr[rn] != -1) {
             arr[rn] = -1;
-            mines.push(rn);
+            mines[rn] = true;
         } else {
             i -= 1;
         }
     }
-    mines.forEach(index => {
+    Object.keys(mines).forEach(index => {
+        nom += 1;
         let i = Math.floor(index / n);
         let j = index % n;
         isValid(index, i, j);
@@ -50,18 +47,42 @@ document.addEventListener('DOMContentLoaded', () => {
                 squares.forEach(index => {
                     index.removeEventListener('click', flip);
                 });
-                mines.forEach(index => {
+                Object.keys(mines).forEach(index => {
                     squares[index].classList.add('mine');
                 });
                 setTimeout(() => {
-                    alert("Game over\nYou landed on a mine");
+                    alert("Game over\nYou're infected\n#RIP");
                 }, 100);
             }, 200);
         } else if (arr[this.id] != 0) {
             showNum(this.id);
         } else {
             getOthers(this.id);
-            console.log(squares);
+        }
+    }
+
+    function flag() {
+        if (this.classList.contains('flag')) {
+            this.classList.remove('flag');
+            this.addEventListener('click', flip);
+        } else {
+            this.classList.add('flag');
+            this.removeEventListener('click', flip);
+            flagged.unshift(this.id);
+            if (flagged.length === nom) {
+                flagged.forEach(index => {
+                    if ((index in mines)) {
+                        count += 1;
+                    }
+                });
+                setTimeout(() => {
+                    if (count != nom) {
+                        alert("Noob\nYou're infected\n#RIP");
+                    } else {
+                        alert("GG WP");
+                    }
+                }, 100);
+            }
         }
     }
 
@@ -80,7 +101,7 @@ document.addEventListener('DOMContentLoaded', () => {
             col = "brown";
         }
         document.getElementById(index).style.borderColor = "lightgrey";
-        squares[index].setAttribute('class', 'none');
+        squares[index].classList.add('none');
         if (temp != 0 && temp != n * n) {
             squares[index].innerHTML = temp;
             document.getElementById(index).style.color = col;
@@ -93,9 +114,9 @@ document.addEventListener('DOMContentLoaded', () => {
             squares[index].removeEventListener('click', flip);
             squares[index].removeEventListener('contextmenu', flag);
             if (arr[index] == 0) {
+                arr[index] = n * n;
                 let i = Math.floor(index / n);
                 let j = index % n;
-                arr[index] = n * n;
                 if (i + 1 < n) {
                     getOthers((i + 1) * n + j);
                 }
